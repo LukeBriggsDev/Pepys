20e508dc0787d4040ccafaa47f15ff4cb453ca73
Luke Briggs <c0028568@newcastle.ac.uk>
2021-04-04 10:44:06 +0100

    Add italic formatting in edit pane

--- src/main/python/EditPane.py
+++ src/main/python/EditPane.py
@@ -15,9 +15,30 @@ class EditPane(AbstractPane.AbstractPane):
 
     def applyFormatting(self):
         currentCaretPosition = self.textCursor().position()
-        headerPattern = re.compile("^ *(#{1,6}) *([^\n]+?) *#* *(?:\n+|$)", re.MULTILINE)
-        headers = [x for x in re.finditer(headerPattern, self.toPlainText())]
         sliderPos = self.verticalScrollBar().sliderPosition()
+
+        textPattern = re.compile("[\s\S]+?(?=[\\<!\[_*`~]|https?://| {2,}\n|$)")
+
+        headerPattern = re.compile("^ *(#{1,6}) *([^\n]+?) *#* *(?:\n+|$)", re.MULTILINE)
+        emphasisPattern =     emphasis = re.compile(
+            r'^\b_((?:__|[^_])+?)_\b'  # _this_
+            r'|'
+            r'^\*((?:\*\*|[^\*])+?)\*(?!\*)'  # *this*
+        )
+
+        text = [x for x in re.finditer(textPattern, self.toPlainText())]
+        for match in text:
+            cursor = QtGui.QTextCursor(self.document())
+            cursor.setPosition(match.start())
+            cursor.setPosition(match.end(), self.textCursor().KeepAnchor)
+            self.setTextCursor(cursor)
+            formatter = QtGui.QTextCharFormat()
+            formatter.setFontWeight(QtGui.QFont.Normal)
+            formatter.setFontItalic(False)
+            cursor.mergeCharFormat(formatter)
+
+
+        headers = [x for x in re.finditer(headerPattern, self.toPlainText())]
         print("WOO")
         for header in headers:
             cursor = QtGui.QTextCursor(self.document())
@@ -28,8 +49,20 @@ class EditPane(AbstractPane.AbstractPane):
             formatter.setFontWeight(QtGui.QFont.Bold)
             cursor.mergeCharFormat(formatter)
 
+        emphasis = [x for x in re.finditer(emphasisPattern, self.toPlainText())]
+
+        for emphasis in emphasis:
+            cursor = QtGui.QTextCursor(self.document())
+            cursor.setPosition(emphasis.start())
+            cursor.setPosition(emphasis.end(), self.textCursor().KeepAnchor)
+            self.setTextCursor(cursor)
+            formatter = QtGui.QTextCharFormat()
+            formatter.setFontItalic(True)
+            cursor.mergeCharFormat(formatter)
+
         formatter = QtGui.QTextCharFormat()
         formatter.setFontWeight(QtGui.QFont.Normal)
+        formatter.setFontItalic(False)
         cursor = QtGui.QTextCursor(self.document())
         cursor.setPosition(currentCaretPosition)
         cursor.mergeCharFormat(formatter)
@@ -41,7 +74,7 @@ class EditPane(AbstractPane.AbstractPane):
         self.currentFile = filepath
 
     def keyReleaseEvent(self, e:QtGui.QKeyEvent) -> None:
-        allowedUpdateChars = [' ', '\n', '\r']
+        allowedUpdateChars = [' ', '\n', '\r', chr(8)]
         if e.text().isalnum() or e.text() in allowedUpdateChars:
             self.applyFormatting()
 
