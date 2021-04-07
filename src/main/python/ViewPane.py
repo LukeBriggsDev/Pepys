d20faf88dbc2f71df7b7807560294884321e8680
Luke Briggs <c0028568@newcastle.ac.uk>
2021-04-07 15:15:37 +0100

    Change ViewPane to QTextBrowser to allow for hyperlink clicking

--- src/main/python/ViewPane.py
+++ src/main/python/ViewPane.py
@@ -8,14 +8,26 @@ if typing.TYPE_CHECKING:
 import regex
 
 
-class ViewPane(AbstractPane.AbstractPane):
+class ViewPane(QtWidgets.QTextBrowser):
     """Pane for viewing rendered markdown in."""
+
     def __init__(self, ctx: AppContext) -> None:
         """Initialise ViewPane.
 
         :param ctx: current ApplicationContext
         """
-        super().__init__(ctx)
+
+        super().__init__()
+        filename = ctx.get_resource("PaneStyle.qss")
+        with open(filename) as file:
+            stylesheet = file.read()
+        self.setWordWrapMode(QtGui.QTextOption.WrapAtWordBoundaryOrAnywhere)
+        self.setStyleSheet(stylesheet)
+        self.setVerticalScrollBarPolicy(self.verticalScrollBarPolicy().ScrollBarAlwaysOn)
+
+        self.setOpenExternalLinks(True)
+
+
         self.setReadOnly(True)
 
         filename = ctx.get_resource("ViewPaneStyle.qss")
@@ -28,11 +40,48 @@ class ViewPane(AbstractPane.AbstractPane):
         self.setTextInteractionFlags(QtGui.Qt.TextBrowserInteraction)
 
     def update_size(self, new_frame_width: int) -> None:
-        super(ViewPane, self).update_size(new_frame_width)
+
+        # Increase width of scroll bar left border to create a margin 25% width of the main window.
+        margin_scale = 1 / 4
+        scroll_bar_width = 4
+        self.verticalScrollBar().setStyleSheet(
+            "QScrollBar:vertical {"
+            f"width: {new_frame_width * margin_scale + scroll_bar_width};"
+            f"border-left-width: {str(new_frame_width * margin_scale - scroll_bar_width)}px ;"
+            "}")
+
+        # Increase left border of the QTextEdit pane to create a left margin 25% width of the main window
+        self.setStyleSheet(self.styleSheet() +
+                           "QTextEdit { "
+                           f"border-left-width: {new_frame_width * margin_scale} px;"
+                           "}")
+
         # TODO: Make resizing large images less laggy
         html = regex.sub(r'(?<=<img[^>]*)(width="[\d.]+")', f'width="{self.width() * 0.5}" ',
                          self.toHtml())
         self.setHtml(html)
+
+        # TODO: Make resizing large images less laggy
+        html = regex.sub(r'(?<=<img[^>]*)(width="[\d.]+")', f'width="{self.width() * 0.5}" ',
+                         self.toHtml())
+        self.setHtml(html)
+
+    def enterEvent(self, event: QtCore.QEvent) -> None:
+        """"Override base QTextEdit method, called when mouse is over TextEdit
+
+        :param event: the QEvent that caused the invocation
+        """
+
+        self.verticalScrollBar().setVisible(True)
+
+    def leaveEvent(self, event: QtCore.QEvent) -> None:
+        """"Override base QTextEdit method, called when mouse leaves TextEdit
+
+        :param event: the QEvent that caused the invocation
+        """
+
+        self.verticalScrollBar().setVisible(False)
+
 
 
 
