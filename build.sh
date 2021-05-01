f634689a41ca3f10269128821e057f2bd48ec2c7
Luke Briggs <lukebriggs02@gmail.com>
2021-05-01 15:33:36 +0100

    Fix build.sh not including pyinstaller argument for venv

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 if command -v pyinstaller; then
   pyinstaller unix.spec
-elif ! [ -n "$(find ./ -type f -iname pyinstaller)" ]; then
-  find ./ -type f -iname pyinstaller -exec {} \;
+elif [ -n "$(find ./ -type f -iname pyinstaller)" ]; then
+  eval "$(find ./ -type f -iname pyinstaller) unix.spec";
 else
   echo "No pyinstaller detected";
   exit 1;
