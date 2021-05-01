8bdc030be22de66edcd20c9fc198bdf0800bdec2
Luke Briggs <lukebriggs02@gmail.com>
2021-05-01 16:08:41 +0100

    Remove all references to GB locale in generated desktop file

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ printf "[Desktop Entry]\n \
 Type=Application\n \
 Name=Pepys\n \
 Categories=Office;\n \
-X-GNOME-FullName[en_GB.UTF-8]=Pepys\n \
-Comment[en_GB.UTF-8]=A straightforward markdown journal\n \
+X-GNOME-FullName=Pepys\n \
+Comment=A straightforward markdown journal\n \
 Icon=%s/.local/share/Pepys/resources/base/icons/appicons/icon.svg\n \
 NoDisplay=false\n \
 Exec=%s/.local/share/Pepys/Pepys\n \
