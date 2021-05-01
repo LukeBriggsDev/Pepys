9408da6089130065684e0d9689905898bbdb45ca
Luke Briggs <lukebriggs02@gmail.com>
2021-05-01 15:17:16 +0100

    Add check for venv pyinstaller within directory

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env sh
-pyinstaller unix.spec
+if command -v pyinstaller; then
+  pyinstaller unix.spec
+elif ! [ -n "$(find ./ -type f -iname pyinstaller)" ]; then
+  find ./ -type f -iname pyinstaller -exec {} \;
+else
+  echo "No pyinstaller detected";
+  exit 1;
+fi
 cp ./install.sh ./dist/
