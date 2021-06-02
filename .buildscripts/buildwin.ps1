49bc155570250285537ed67289004484fb7cbb3c
Luke Briggs <lukebriggs02@gmail.com>
2021-06-02 17:09:43 +0100

    Remove unnecessary build scripts

--- .buildscripts/buildwin.ps1
+++
@@ -1,7 +0,0 @@
-Import-Module ps2exe
-rm -Recurse ..\.win-build
-mkdir ..\.win-build
-cp -Recurse ..\src ..\.win-build
-cp -Recurse ..\venv ..\.win-build
-cp .\installer.nsi ..\.win-build
-ps2exe .\runPepys.ps1 ..\.win-build\Pepys.exe -noConsole -noOutput -noError -iconFile ..\.win-build\src\main\resources\base\icons\appicons\pepys-icon.ico
--- .buildscripts/runPepys.ps1
+++
@@ -1,1 +0,0 @@
-.\venv\Scripts\python.exe .\src\main\python\main.py
