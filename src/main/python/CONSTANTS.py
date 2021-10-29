8c57ef587e86f8a5b24339c86569944a2e8b8dd5
Luke Briggs <lukebriggs02@gmail.com>
2021-10-29 17:14:50 +0100

    Update version number and flatpak dependencies

--- src/main/python/CONSTANTS.py
+++ src/main/python/CONSTANTS.py
@@ -92,7 +92,7 @@ theme = "light"
 with open(get_resource("colors.json")) as colors:
     colors = json.loads(colors.read())
 
-version = "1.1.1"
+version = "1.2.0"
 
 light_palette = QtWidgets.QApplication.palette()
 if not sys.platform.startswith("linux"):
