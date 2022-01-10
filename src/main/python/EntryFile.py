3dc89c4e9a4cba6f5b8ccbdcf72e1fa95159469d
uhrfra <uhrfra@posteo.de>
2022-01-10 20:16:03 +0100

    Add encryption functionality to EntryFile

--- src/main/python/EntryFile.py
+++ src/main/python/EntryFile.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, datetime
 from genericpath import exists
 import json
 import locale
@@ -8,6 +8,7 @@ import os
 import pathlib
 import regex
 import yaml
+from Crypto import Crypto
 
 from CONSTANTS import get_resource
 
@@ -15,12 +16,16 @@ from CONSTANTS import get_resource
 class EntryFile():
     def __init__(self, file_date: date = None) -> None:
 
+        self._crypto = Crypto()
         if file_date is None:
             self._formatted_date = ""
+            self._date = None
             self._directory = ""
             self._long_date = ""
+            self._filename = ""
             return
 
+        self._date = file_date
         self._formatted_date = file_date.strftime("%Y-%m-%d")
         
         config_file = get_resource("config.json")
@@ -28,6 +33,7 @@ class EntryFile():
             config_dict = json.loads(file.read())
             diary_directory = config_dict["diary_directory"]
             self._flat_structure = ("use_flat_directory_structure" in config_dict) and config_dict["use_flat_directory_structure"]
+            encryption_default = ("encrypt_as_default" in config_dict) and config_dict["encrypt_as_default"]
         
         month_directory = os.path.join(diary_directory, str(file_date.year), f"{file_date.month:02}")
         
@@ -39,20 +45,24 @@ class EntryFile():
             if os.path.exists(back_compat_directory):
                 self._directory = os.path.join(back_compat_directory, self._formatted_date)
 
-        if self.exists():
+        self._filename = self.__check_for_existing_filename()
+        if self._filename:
             # File already exists at the default location
             self._flat_structure = False
         else:
             # Check if file already exists with flat directory structure
             default_directory = self._directory
             self._directory = os.path.join(diary_directory, str(file_date.year))
-            if self.exists():
+            self._filename = self.__check_for_existing_filename()
+            if self._filename:
                 self._flat_structure = True
             else:
                 # For a new file select the file location from the settings
                 if self._flat_structure == False:
                     self._directory = default_directory
-
+                self._filename = f"{self._formatted_date}.md"
+                if encryption_default:
+                    self._filename = self._filename + ".crypt"
 
          # Add ordinal to end of number if it exists
         try:
@@ -67,19 +77,89 @@ class EntryFile():
         """The path to the directory of the entry file"""
         return self._directory
 
+
+    @property 
+    def filename(self):
+        """The name of the entry file with extension"""
+        return self._filename
+
+
     @property
     def path(self):
         """The full path to the entry file"""
-        return os.path.join(self._directory, f"{self._formatted_date}.md")
+        return os.path.join(self._directory, self._filename)
 
+
+    @property
+    def date(self):
+        return self._date
 
     @property
     def formatted_date(self):
         return self._formatted_date
 
+
     @property
     def long_date(self):
         return self._long_date
+
+
+    def update(self):
+        self.__init__(self.date)
+
+    def is_encrypted(self):
+        return self._filename.endswith(".crypt")
+
+
+    def set_to_unencrypted(self):
+        if not self.is_encrypted():
+            return
+
+        file_existed = False
+        if self.exists():
+            file_existed = True
+            content = self.get_content()
+            prev_path = self.path
+
+        self._filename = self._filename[:-6]
+       
+        if file_existed:
+            self.save(content)
+            os.remove(prev_path)
+
+
+    def set_to_encrypted(self):
+        if self.is_encrypted():
+            return
+
+        file_existed = False
+        if self.exists():
+            file_existed = True
+            content = self.get_content()
+            prev_path = self.path
+
+        self._filename = self._filename + ".crypt"
+       
+        if file_existed:
+            self.save(content)
+            os.remove(prev_path)
+
+
+    def __check_for_existing_filename(self):
+        if not self.directory_exists():
+            return False
+
+        path = os.path.join(self._directory, f"{self._formatted_date}.md")
+        path_encrypted = os.path.join(self._directory, f"{self._formatted_date}.md.crypt")
+
+        if os.path.exists(path):
+            return f"{self._formatted_date}.md"
+
+        if os.path.exists(path_encrypted):
+            return f"{self._formatted_date}.md.crypt"
+
+        return ""
+
 
     def get_header_text(self):
         return '---\n'\
@@ -88,25 +168,33 @@ class EntryFile():
         'tags: []\n'\
         '---\n'
 
+
     def get_content(self):
         if not self.exists():
             return ""
 
         with open(self.path, "r+") as file:
-                return file.read()
+            content = file.read()
+            if self.is_encrypted():
+                content = self._crypto.decrypt(content)
+            return content
+
 
     def directory_exists(self):
         if self._directory == "":
             return False
         return os.path.exists(self._directory)
 
+
     def exists(self):
         if not self.directory_exists():
             return False
         return os.path.exists(self.path)
 
+
     def create_directory(self):
          pathlib.Path(self._directory).mkdir(parents=True, exist_ok=True)
+
 
     def save(self, content: str):
         if content == self.get_header_text():
@@ -115,9 +203,13 @@ class EntryFile():
 
         if not self.directory_exists():
             self.create_directory()
+
+        if (self.is_encrypted()):
+            content = self._crypto.encrypt(content)
         
         with open(self.path, "w+") as file:
             file.write(content)
+
 
     def get_tags(self):
         content = self.get_content()
@@ -131,6 +223,7 @@ class EntryFile():
                 return meta_dict["tags"]
         except Exception:
             return {}
+
 
     def copy_image(self, image_path):
         """Copies the image to the local diary entry folder and returns the relative path to the copy"""
@@ -151,10 +244,22 @@ class EntryFile():
 def get_all_entry_files():
     with open(get_resource("config.json"), "r") as file:
         directory = json.loads(file.read())["diary_directory"]
-    return list(pathlib.Path(directory).rglob("*-*-*.[mM][dD]"))
+    files = list(pathlib.Path(directory).rglob("*-*-*.[mM][dD]"))
+    files.extend(list(pathlib.Path(directory).rglob("*-*-*.[mM][dD].crypt")))
+
+    ret = list()
+    for file in files:
+        fn = file.name;
+        fn = fn.replace(".crypt", "")
+        fn = fn[:-3]
+        date = datetime.strptime(fn, "%Y-%m-%d");
+        ret.append(EntryFile(date))
+
+    return ret
+
 
 def get_all_entry_files_in_range(startdate: date, enddate: date):
     diary_entries = get_all_entry_files()
     diary_entries = [entry for entry in diary_entries
-                            if startdate.strftime("%Y-%m-%d") <= entry.name[:-3] <= enddate.strftime("%Y-%m-%d")]
+                            if startdate <= entry.date <= enddate]
     return diary_entries
