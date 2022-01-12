4890cddc0f87505d222cc95f0ad580bc22dc9213
uhrfra <uhrfra@posteo.de>
2022-01-12 20:52:46 +0100

    Compute password hash of crypto key
    ... instead of hashing password to increase security

--- src/main/python/Crypto.py
+++ src/main/python/Crypto.py
@@ -12,7 +12,7 @@ class Crypto:
 
     def __init__(self, password = None):
         if Crypto._key == None and password:
-            Crypto._key = self.__generateKeyFromPassword(password)
+            Crypto._key = generateKeyFromPassword(password)
 
 
     def is_initialized(self):
@@ -32,14 +32,15 @@ class Crypto:
         return Fernet(self._key).decrypt(base64.urlsafe_b64decode(text)).decode()
 
 
-    def __generateKeyFromPassword(self, password: str):
-        # Always the same salt that only the password is used as input for encryption key
-        salt = b'y\x01\x9e\x02\x13\xa93\x13;u6\xcc\xef\xad\xc1\x92'
-        kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=32, salt=salt, iterations=100000, backend=default_backend())
-        return base64.urlsafe_b64encode(kdf.derive(password.encode()))
+def generateKeyFromPassword(password: str):
+    # Always the same salt that only the password is used as input for encryption key
+    salt = b'y\x01\x9e\x02\x13\xa93\x13;u6\xcc\xef\xad\xc1\x92'
+    kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=32, salt=salt, iterations=100000, backend=default_backend())
+    return base64.urlsafe_b64encode(kdf.derive(password.encode()))
 
 
 def generateVerificationStringFromPassword(password: str):
-    digest = hashes.Hash(hashes.SHA1(), backend=default_backend())
-    digest.update(password.encode())
+    key = generateKeyFromPassword(password)
+    digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
+    digest.update(key)
     return base64.urlsafe_b64encode(digest.finalize()).decode()
