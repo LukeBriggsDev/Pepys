751a4702424b60970de90ffebb28cf11993d2aa5
uhrfra <uhrfra@posteo.de>
2022-01-25 23:03:28 +0100

    Perform search in a background thread
    This does not block the GUI searching large diaries.
    The search can be interrupted by the user.

--- src/main/python/SearchWidget.py
+++ src/main/python/SearchWidget.py
@@ -8,6 +8,63 @@ from EditPane import EditPane
 from WebView import WebView
 from EntryFile import get_all_entry_files
 
+# Backgournd worker class perfoming search
+class SearchWorker(QtCore.QObject):
+    finished = QtCore.pyqtSignal()
+    progress = QtCore.pyqtSignal(int)
+    result_found = QtCore.pyqtSignal(list)
+
+    def __init__(self, searchText):
+        super().__init__()
+        self._searchText = searchText
+        self._is_running = False
+        self._break = False
+
+    def interrupt(self):
+        self._break = True
+
+    def is_running(self):
+        return self._is_running
+
+    def run(self):
+        self._break = False
+        self._is_running = True
+        entry_files = get_all_entry_files()
+        entry_files.sort(key=lambda x: x.formatted_date)
+
+        results = []
+        counter = 0
+        for e in entry_files:
+            if self._break:
+                break
+            counter = counter + 1
+            self.progress.emit(int(counter *100/ len(entry_files)))
+            table_text = self._build_table_text(e.get_content())
+            if table_text:
+                self.result_found.emit([e.formatted_date, table_text])
+
+        self.finished.emit()
+        self._is_running = False
+
+    def _build_table_text(self, entryText):
+        match_positions = [m.start() for m in re.finditer(self._searchText, entryText, re.IGNORECASE)]
+
+        if len(match_positions) == 0:
+            return ""
+
+        pos = match_positions[0]
+
+        end = pos + 78
+        if end >= len(entryText):
+            table_text = entryText[pos:]
+        else:
+            table_text = entryText[pos:end] + "..."
+
+        if len(match_positions) > 1:
+            table_text = table_text + "(and " + str(len(match_positions)-1) + " more)"
+
+        return table_text
+
 
 class SearchWidget(QtWidgets.QWidget):
     def __init__(self, edit_pane: EditPane, web_view: WebView):
@@ -15,16 +72,17 @@ class SearchWidget(QtWidgets.QWidget):
 
         self.edit_pane = edit_pane
         self.web_view = web_view
+        self.worker = None
 
         self.searchText = QtWidgets.QLineEdit(self)
         self.searchText.returnPressed.connect(self.search_clicked)
 
-        searchButton = QtWidgets.QPushButton("Search", self)
-        searchButton.clicked.connect(self.search_clicked)
+        self.searchButton = QtWidgets.QPushButton("Search", self)
+        self.searchButton.clicked.connect(self.search_clicked)
 
         toolbar = QtWidgets.QToolBar(self)
         toolbar.addWidget(self.searchText)
-        toolbar.addWidget(searchButton)
+        toolbar.addWidget(self.searchButton)
 
         self.progress_bar = QtWidgets.QProgressBar()
         self.progress_bar.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
@@ -52,53 +110,32 @@ class SearchWidget(QtWidgets.QWidget):
 
 
     def search_clicked(self):
+        if self.worker and self.worker.is_running():
+            self.worker.interrupt()
+            return
+
+        self.progress_bar.setValue(0)
         self.progress_bar.setVisible(True)
 
         if self.searchText.text() == "":
             return
 
-        entry_files = get_all_entry_files()
-        entry_files.sort(key=lambda x: x.formatted_date)
-
-        results = []
-        counter = 0
-        for e in entry_files:
-            counter = counter + 1
-            self.progress_bar.setValue(int(counter *100/ len(entry_files)))
-            table_text = self.build_table_text(e.get_content(), self.searchText.text())
-            if table_text:
-                results.append([e.formatted_date, table_text])
-
         self.table.clearContents()
-        self.table.setRowCount(len(results))
-        counter = 0
-        for r in results:
-            self.table.setItem(counter, 0, QtWidgets.QTableWidgetItem(r[0]),)
-            self.table.setItem(counter, 1, QtWidgets.QTableWidgetItem(r[1]))
-            counter = counter + 1
-        
-        self.progress_bar.setVisible(False)
-        self.table.resizeColumnsToContents()
+        self.table.setRowCount(0)
 
+        # Run search in a background thread
+        self.thread = QtCore.QThread()
+        self.worker = SearchWorker(self.searchText.text())
+        self.worker.moveToThread(self.thread)
+        self.thread.started.connect(self.worker.run)
+        self.worker.finished.connect(self.search_finished)
+        self.worker.finished.connect(self.thread.quit)
+        self.worker.result_found.connect(self.result_found)
+        self.worker.progress.connect(self.update_progress)
+        self.thread.start()
 
-    def build_table_text(self, entryText, searchText):
-        match_positions = [m.start() for m in re.finditer(searchText, entryText, re.IGNORECASE)]
-
-        if len(match_positions) == 0:
-            return ""
-
-        pos = match_positions[0]
-
-        end = pos + 78
-        if end >= len(entryText):
-            table_text = entryText[pos:]
-        else:
-            table_text = entryText[pos:end] + "..."
-
-        if len(match_positions) > 1:
-            table_text = table_text + "(and " + str(len(match_positions)-1) + " more)"
-
-        return table_text
+        self.searchButton.setText("Stop")
+        self.searchText.setEnabled(False)
 
 
     def on_row_selected(self):
@@ -107,6 +144,25 @@ class SearchWidget(QtWidgets.QWidget):
             date = datetime.datetime.strptime(formatted_date, "%Y-%m-%d")
             self.edit_pane.open_file_from_date(date)
             self.web_view.refresh_page()
+
+
+    def update_progress(self, progress):
+        self.progress_bar.setValue(progress)
+
+
+    def result_found(self, result):
+        row_index = self.table.rowCount()
+        self.table.insertRow(row_index)
+        self.table.setItem(row_index, 0, QtWidgets.QTableWidgetItem(result[0]),)
+        self.table.setItem(row_index, 1, QtWidgets.QTableWidgetItem(result[1]))
+        self.table.resizeColumnsToContents()
+
+
+    def search_finished(self):
+        self.progress_bar.setVisible(False)
+        self.searchButton.setText("Search")
+        self.searchText.setEnabled(True)
+
 
     def showEvent(self, e):
         self.searchText.setFocus()
