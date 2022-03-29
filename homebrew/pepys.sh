4f5d8406f1da5e09a54d63c76802724a812526f6
Luke Briggs <lukebriggs02@gmail.com>
2022-03-29 13:46:29 +0100

    Update homebrew ruby file

--- homebrew/pepys.sh
+++ homebrew/pepys.sh
@@ -2,4 +2,4 @@ source "$HOME"/.zshrc
 export HOMEBREW_CELLAR=$(brew --cellar)
 export LANG="$(defaults read -g AppleLanguages | \
   sed '/"/!d;s/["[:space:]]//g;s/-/_/').UTF-8"
-$HOMEBREW_CELLAR/pepys/1.5.0/libexec/bin/python3 $HOMEBREW_CELLAR/pepys/1.5.0/lib/main/python/main.py
+$HOMEBREW_CELLAR/pepys/1.5.2/libexec/bin/python3 $HOMEBREW_CELLAR/pepys/1.5.2/lib/main/python/main.py
